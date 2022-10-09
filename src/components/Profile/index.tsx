b0c330ce8e2d7a43498737090c54f4731a7e14ab
corpp02535 <brunohorn@ext.cvccorp.com.br>
2022-10-08 22:11:16 -0300

    Criando o login com discord

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,23 +1,59 @@
-import { CaretDown } from 'phosphor-react';
+import { useState } from 'react';
+import { CaretDown, UserCircle } from 'phosphor-react';
+import axios from 'axios';
+import Link from 'next/link';
+
+interface DiscordLoginProps {
+  clientId: string;
+  redirectUri: string;
+  scope?: string;
+  render?: any;
+}
 
 export function Profile() {
+  const [user, setUser] = useState('');
+  const [loginModal, setLoginModal] = useState(false);
+
+  const discordLogin = async () => {
+    const retorno = await axios.get('/api/discord/login');
+    console.log('RETORNO: ', retorno);
+  };
+
   return (
     <div
       className='flex gap-2 p-2 items-center cursor-pointer hover:bg-[#1d1a2496] rounded'
       title='Perfil'
     >
-      <div className='flex flex-col items-end'>
-        <span className='text-white font-semibold text-sm'>Bruno</span>
+      {user === '' ? (
+        <Link href={'http://localhost:3000/api/discord/login'}>
+          <button className='flex gap-2'>
+            <div className='flex flex-col items-end'>
+              <span className='text-white font-semibold text-sm'>
+                Efetue o login
+              </span>
 
-        <span className='text-zinc-500 text-xs'>#brunofhorn</span>
-      </div>
+              <span className='text-zinc-500 text-xs'>no Discord</span>
+            </div>
 
-      <img
-        className='w-10 h-10 rounded'
-        src={`https://cdn.discordapp.com/avatars/`}
-      />
+            <UserCircle size={32} color='#FFF' weight='bold' />
+          </button>
+        </Link>
+      ) : (
+        <>
+          <div className='flex flex-col items-end'>
+            <span className='text-white font-semibold text-sm'>Bruno</span>
 
-      <CaretDown size={16} color='#FFF' weight='bold' />
+            <span className='text-zinc-500 text-xs'>#brunofhorn</span>
+          </div>
+
+          <img
+            className='w-10 h-10 rounded'
+            src={`https://cdn.discordapp.com/avatars/`}
+          />
+
+          <CaretDown size={16} color='#FFF' weight='bold' />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/pages/api/discord/login/index.tsx
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const OAuthScope = ['identify'].join(' ');
+const OAuthData = new URLSearchParams({
+  response_type: 'code',
+  client_id: process.env.CLIENT_ID as string,
+  redirect_uri: `http://localhost:3000/api/discord/callback`,
+  scope: OAuthScope,
+});
+
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.redirect(`https://discordapp.com/oauth2/authorize?${OAuthData}`);
+}
