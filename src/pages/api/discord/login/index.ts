63fa219eac2ee835383d3b4061e0cbf2e775d6f3
corpp02535 <brunohorn@ext.cvccorp.com.br>
2022-10-10 11:03:09 -0300

    encode URI

--- src/pages/api/discord/login/index.ts
+++ src/pages/api/discord/login/index.ts
@@ -4,7 +4,7 @@ const OAuthScope = ['identify'].join(' ');
 const OAuthData = new URLSearchParams({
   response_type: 'code',
   client_id: process.env.CLIENT_ID as string,
-  redirect_uri: `${process.env.DOMAIN}/api/discord/callback`,
+  redirect_uri: encodeURI(`${process.env.DOMAIN}/api/discord/callback`),
   scope: OAuthScope,
 });
 
