40b6c6abab194f268cb6da125cc688acdabdb75b
corpp02535 <brunohorn@ext.cvccorp.com.br>
2022-10-10 11:26:11 -0300

    URL de callback

--- src/pages/api/discord/login/index.ts
+++ src/pages/api/discord/login/index.ts
@@ -4,9 +4,7 @@ const OAuthScope = ['identify'].join(' ');
 const OAuthData = new URLSearchParams({
   response_type: 'code',
   client_id: process.env.CLIENT_ID as string,
-  redirect_uri: decodeURIComponent(
-    `${process.env.DOMAIN}/api/discord/callback`
-  ),
+  redirect_uri: 'https://duo-esports-ignite.vercel.app/api/discord/callback',
   scope: OAuthScope,
 });
 
