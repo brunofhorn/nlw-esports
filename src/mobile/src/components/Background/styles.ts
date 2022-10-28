c2d08f69b00c4d19d1625fb5e151bfc4e3ab66f8
brunofhorn <b.fernandeshorn@gmail.com>
2022-10-28 12:02:36 -0300

    remoção do mobile

--- src/mobile/src/components/Background/styles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-import { THEME } from '../../theme';
-
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: THEME.COLORS.BACKGROUND_800
-  }
-});
