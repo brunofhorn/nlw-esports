964cf94dafbe16994e7d3bdef0ebc020431365c9
corpp02535 <brunohorn@ext.cvccorp.com.br>
2022-11-13 21:11:59 -0300

    404 page

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -1,6 +1,6 @@
 import { Header } from '@components/Header';
 import Link from 'next/link';
-import { Ghost } from 'phosphor-react';
+import { Ghost, ArrowLeft } from 'phosphor-react';
 
 function Custom404() {
   return (
@@ -8,12 +8,12 @@ function Custom404() {
       <div className='max-w-[1344px] flex flex-col items-center my-0 mx-2 sm:mx-20 sm:my-20'>
         <Header />
         <div className='text-center mt-10 justify-center items-center'>
-          <Ghost size={60} color='white' />
+          <Ghost size={60} color='white' className='m-auto' />
           <div className='text-3xl font-semibold mb-10 text-white'>
             Ops! Página não encontrada.
           </div>
-          <Link href='/' className='bg-violet-400 p-2 text-white'>
-            Voltar
+          <Link href='/'>
+            <a className='bg-purple-500 p-2 text-white rounded'>Voltar</a>
           </Link>
         </div>
       </div>
